7eec9b7f214d16d5b1d2c5197b1b64030fd0a4f5
MessilineHani <pleasefocus.exe@gmail.com>
2023-05-20 14:47:56 +0100

    Initial commit

--- main.js
+++
@@ -1,5 +0,0 @@
-const barsButton = document.getElementById("bars-button");
-let mainContainer = document.querySelector(".main-container");
-barsButton.onclick = () =>{
-    mainContainer.classList.add("show-menu");
-}
